558ce5a61fce125c5d3ccc0551ee08a2e972b040
NurKeinNeid <nurkeinneid@derpfest.org>
2025-02-22 16:14:01 +0100

    SystemUI: Add ongoing action chip toggle setting [1/2]
    Change-Id: I2083b5184d9a8233722661e42d7d5f33876fc276
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: IHSAN UL RAHMAN <ihsanulrahman@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6526,6 +6526,11 @@ public final class Settings {
         public static final String SCREEN_FLASH_NOTIFICATION_COLOR =
                 "screen_flash_notification_color_global";
 
+        /**
+          * @hide
+          */
+         public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
+ 
 	/**
          * Color temperature of the display during the day
          * @hide
